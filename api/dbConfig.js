4748abf2e92c3bdfd0830050cc70ee24fb851d2b
Kelvin Stephano <kstephano@gmail.com>
2021-12-16 18:31:36 +0000

    connection to mongo client called at server start

--- api/dbConfig.js
+++ api/dbConfig.js
@@ -3,22 +3,21 @@ const { MongoClient } = require('mongodb');
 const dbUrl = process.env.DB_CONNECTION; // connection url
 const dbName = process.env.DB_NAME; // db name
 
-async function initConnection() {
-    // Create a new MongoClient
-    const client = new MongoClient(dbUrl);
-    // Connect the client to the server
-    await client.connect();
+// Create a new MongoClient
+const client = new MongoClient(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true });   
 
-    return client;
+async function initConnection() {
+    // Connect the client to the server
+    console.log('connected to mongo');
+    await client.connect();
 }
 
 async function initDB() {
-    const client = await initConnection();
     console.log(`connected to database ${dbName}`);
     return client.db(dbName);
 }
 
 module.exports = { 
-    initConnection,
-    initDB 
+    initDB,
+    initConnection
 };
